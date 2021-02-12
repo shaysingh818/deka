50687ea1c12c9c4822a6711f781657f84134c283
shaysingh818 <shalinsingh818@gmail.com>
2021-02-11 21:34:22 -0500

    Added auto schema generation for client db

--- cli_tool/client.py
+++ cli_tool/client.py
@@ -17,7 +17,7 @@ class Client:
         self.questions = [] 
         self.alias = alias
         self.db_file = "{}.db".format(alias) 
-        self.db_path = db_dir + "/db/{}".format(self.db_file)  
+        self.db_path = db_dir + "/db/{}".format(self.db_file)
 
     # database helper method
     def init_db(self):
@@ -25,16 +25,47 @@ class Client:
         try:
             self.conn = sqlite3.connect(self.db_path) 
         except Exception as e:
-            print(e) 
+            print(e)
+ 
+    def check_status(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        fetch = cursor.fetchall() 
+        if len(fetch) == 0:
+            command = "cat {}/db/client.sql | sqlite3 {}/db/{}".format(db_dir, db_dir, self.db_file)
+            os.system(command) 
+            print("creating database: ")
+        else:
+            print("Schema already generated: ")  
 
-"""
+        self.conn.close() 
+        
+    """ 
+    def generate_account_key(self, service, key): 
+        data = (service, key)
+        sql = '''  INSERT INTO account_keys(service,key) VALUES (?, ?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, data) 
+        self.conn.commit()
+        print("inserted account private key: {} ".format(key)) 
+        self.conn.close()
+ 
+    def generate_security_question_key(self, question, key, account_id):
+        data = (question,key, account_id)
+        sql = '''  INSERT INTO question_keys(question,key,question_account) VALUES (?, ?, ?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, data) 
+        self.conn.commit()
+        print("inserted security question key: {} ".format(key))
+        self.conn.close() 
 #create database client
 myClient = Client(
     "http://127.0.0.1:5000",
-    "client1"
+    "client46"
 )
 
 myClient.init_db()
-""" 
-        
+myClient.check_status()
+
+"""        
 
